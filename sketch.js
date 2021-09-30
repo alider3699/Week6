2b75987188be490914b78450dda7a7a117b06eb1
alider3699 <77299744+alider3699@users.noreply.github.com>
2021-09-30 11:57:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  var myCanvas = createCanvas(1664,500);
+    myCanvas.parent('canvasDiv');
+   background(0);
+}
+
+function draw() {
+  stroke(255);
+  fill(240,248,255,1);
+  
+    line(mouseX,mouseY, 832,250);
+  
+ 
+}
+
+function mousePressed(){
+  background(0);
+}
